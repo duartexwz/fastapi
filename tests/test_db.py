8d2a2d99d99cc9849a360bb2889238c272d00d7a
duartex_ <mayckonkennedy877@gmail.com>
2026-05-22 13:27:52 -0300

    subindo atualização com banco de dados

---
+++ tests/test_db.py
@@ -0,0 +1,26 @@
+from sqlalchemy import select
+from dataclasses import asdict
+
+
+from api.models import User
+
+
+def test_create_user(session, mock_db_time):
+    with mock_db_time(model=User) as time:
+
+        new_user = User(
+            username='Maria Eduarda', email='ms.mariasilva@gmail.com', password='123456'
+        )
+
+    session.add(new_user)
+    session.commit()
+
+    user = session.scalar(select(User).where(User.username == 'Maria Eduarda'))
+
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'Maria Eduarda',
+        'email': 'ms.mariasilva@gmail.com',
+        'password': '123456',
+        'created_at': time,
+    }
