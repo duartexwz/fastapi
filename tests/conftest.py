b33c906aaf1657eac64e9340a1b78a893e4c149a
duartex_ <mayckonkennedy877@gmail.com>
2026-05-22 13:32:50 -0300

    corrigindo atualização anterior

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,13 @@
+from contextlib import contextmanager
+from datetime import datetime
+
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine, event
 from sqlalchemy.orm import Session
-from datetime import datetime
+
 from api.app import app
 from api.models import table_registry
-
-from contextlib import contextmanager
 
 
 @pytest.fixture
@@ -31,13 +32,14 @@ def _mock_db_time(*, model, time=datetime(2026, 1, 1)):
     def fake_time_hook(mapper, connection, target):
         if hasattr(target, 'created_at'):
             target.created_at = time
+
     event.listen(model, 'before_insert', fake_time_hook)
 
     yield time
 
     event.remove(model, 'before_insert', fake_time_hook)
 
-    
+
 @pytest.fixture
 def mock_db_time():
     return _mock_db_time
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,19 +1,18 @@
-from sqlalchemy import select
 from dataclasses import asdict
 
+from sqlalchemy import select
 
 from api.models import User
 
 
 def test_create_user(session, mock_db_time):
     with mock_db_time(model=User) as time:
-
         new_user = User(
             username='Maria Eduarda', email='ms.mariasilva@gmail.com', password='123456'
         )
 
-    session.add(new_user)
-    session.commit()
+        session.add(new_user)
+        session.commit()
 
     user = session.scalar(select(User).where(User.username == 'Maria Eduarda'))
 
